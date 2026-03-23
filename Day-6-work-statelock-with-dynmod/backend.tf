fc675dfa4c6a31fb206c33ffbf882147287d2209
Palla <Prasad.Palla@mvwc.com>
2026-03-22 20:09:29 -0400

    root child module

--- Day-6-work-statelock-with-dynmod/backend.tf
+++ Day-6-work-statelock-with-dynmod/backend.tf
@@ -6,6 +6,6 @@ terraform {
     # Enable S3 native locking
     #use_lockfile = true   #tf version should be above 1.10
     # The dynamodb_table argument is no longer needed
-    dynamodb_table = "LockID"
+    dynamodb_table = "tf_locks"
   }
 }
---
+++ Day-6-work-statelock-with-dynmod/main.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "prod"
+    }
+  
+}
+
+resource "aws_subnet" "name" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+        Name = "prod-subnet"
+    }
+  
+}
+
+
+
+resource "aws_subnet" "name-1" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+        Name = "prod-subnet"
+    }
+  
+}
